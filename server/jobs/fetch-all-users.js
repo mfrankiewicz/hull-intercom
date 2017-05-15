4e23678f5f8dc37b6ae294b479d958f9363f22a5
Michal Raczka <michal@raczka.me>
2017-05-15 13:51:34 +0200

    Reduce number of queueing operations, use job functions directly

--- server/jobs/fetch-all-users.js
+++ server/jobs/fetch-all-users.js
@@ -1,6 +1,8 @@
 import Promise from "bluebird";
 import _ from "lodash";
 
+import saveUsers from "./save-users";
+
 export default function fetchAllUsers(ctx, payload = {}) {
   const { scroll_param } = payload;
   const { intercomAgent } = ctx.shipApp;
@@ -14,15 +16,9 @@ export default function fetchAllUsers(ctx, payload = {}) {
       if (_.isEmpty(users)) {
         return Promise.resolve();
       }
-      // scroll feature of Intercom API have expiration time which is
-      // hard to recover from. The is the reason why continuing the import
-      // scroll queries is more important than other tasks here.
-      // It will put much more data into the queue, but when user scroll param
-      // expires it cannot be recovered.
-      // @see https://developers.intercom.com/reference#iterating-over-all-users
       return Promise.all([
-        ctx.enqueue("fetchAllUsers", { scroll_param: next_scroll_param }, { priority: "high" }),
-        ctx.enqueue("saveUsers", { users })
+        fetchAllUsers(ctx, { scroll_param: next_scroll_param }),
+        saveUsers(ctx, { users })
       ]);
     });
 }
--- server/jobs/fetch-users.js
+++ server/jobs/fetch-users.js
@@ -2,17 +2,20 @@ import Promise from "bluebird";
 import moment from "moment";
 import _ from "lodash";
 
+import saveUsers from "./save-users";
+
 export default function fetchUsers(ctx, payload = {}) {
   const { intercomAgent } = ctx.shipApp;
   const defaultLastUpdatedAt = ctx.ship.private_settings.last_updated_at || moment().subtract(1, "hour").format();
   const { last_updated_at = defaultLastUpdatedAt, count = 50, page = 1 } = payload;
 
   ctx.client.logger.debug("fetchUsers", { last_updated_at, page });
+  ctx.metric.value("ship.incoming.fetch.page", page);
   return intercomAgent.getRecentUsers(last_updated_at, count, page)
     .then(({ users, hasMore }) => {
       const promises = [];
       if (hasMore) {
-        promises.push(ctx.enqueue("fetchUsers", {
+        promises.push(fetchUsers(ctx, {
           last_updated_at,
           count,
           page: page + 1
@@ -20,12 +23,12 @@ export default function fetchUsers(ctx, payload = {}) {
       }
 
       if (!_.isEmpty(users)) {
-        promises.push(ctx.enqueue("saveUsers", { users }));
+        promises.push(saveUsers(ctx, { users }));
       }
 
       return Promise.all(promises)
         .then(() => {
-          const newLastUpdatedAt = _.get(_.last(users), "updated_at");
+          const newLastUpdatedAt = moment(_.get(_.last(users), "updated_at"), "X").format();
           return ctx.helpers.updateSettings({
             last_updated_at: newLastUpdatedAt
           });
