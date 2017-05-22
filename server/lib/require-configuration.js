565f493d432b39141e264d5131cff634bc9a32c8
Michal Raczka <michal@raczka.me>
2017-05-22 11:38:14 +0200

    Check request context object

--- server/lib/require-configuration.js
+++ server/lib/require-configuration.js
@@ -8,7 +8,7 @@ import { Request, Response, Next } from "express";
  * @param  {Function} next
  */
 export default function requireConfiguration(req: Request, res: Response, next: Next) {
-  if (!req.hull.shipApp.syncAgent.isConfigured()) {
+  if (!req.hull.shipApp || !req.hull.shipApp.syncAgent.isConfigured()) {
     req.hull.client.logger.info("ship is not configured");
     return res.status(403).send("Ship is not configured");
   }
