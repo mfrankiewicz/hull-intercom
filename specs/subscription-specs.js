8b3ab3812c08d90b183af9b507365237ddb31ef9
Michal Raczka <michal@raczka.me>
2017-06-27 12:44:09 +0200

    Upgrade specs

--- specs/subscription-specs.js
+++ specs/subscription-specs.js
@@ -15,47 +15,29 @@ describe("ensure webhook operation", function test() {
     minihull.listen(8001);
     miniintercom.listen(8002);
     server = bootstrap();
-
-    const returnConnector = (req, res) => {
-      res.json({
-        id: "595103c73628d081190000f6",
-        private_settings: {
-          access_token: "intercomABC",
-          webhook_id: "abc-123"
-        }
-      });
-    };
-    minihull.stubPut("/api/v1/595103c73628d081190000f6").callsFake(returnConnector);
-    minihull.stubGet("/api/v1/app").callsFake(returnConnector);
-    minihull.stubGet("/api/v1/595103c73628d081190000f6").callsFake(returnConnector);
-    minihull.stubGet("/api/v1/segments")
-      .callsFake((req, res) => {
-        res.json([]);
-      });
+    minihull.stubConnector({
+      id: "595103c73628d081190000f6",
+      private_settings: {
+        access_token: "intercomABC",
+        webhook_id: "abc-123"
+      }
+    });
   });
 
   it("should retry after ten seconds in case of rate limit", (done) => {
     miniintercom.stubGet("/users")
-      .callsFake((req, res) => {
-        res.json({
-          users: [{
-            id: "intercomUserId",
-            email: "foo@bar.com",
-            updated_at: minihull.moment().format("X")
-          }]
-        });
+      .returnsJson({
+        users: [{
+          id: "intercomUserId",
+          email: "foo@bar.com",
+          updated_at: minihull.moment().format("X")
+        }]
       });
     const stub = miniintercom.stubGet("/subscriptions/abc-123")
-      .onFirstCall().callsFake((req, res) => {
-        res.status(429).end();
-      })
-      .onSecondCall().callsFake((req, res) => {
-        res.status(200).end();
-      });
+      .onFirstCall().returnsStatus(429)
+      .onSecondCall().returnsStatus(200);
     miniintercom.stubPost("/subscriptions/abc-123")
-      .callsFake((req, res) => {
-        res.json({});
-      });
+      .returnsJson({});
 
     miniintercom.on("incoming.request@/subscriptions/abc-123", (req) => {
       if(miniintercom.requests.get("incoming").filter({ url: "/subscriptions/abc-123" }).size().value()) {
@@ -63,7 +45,7 @@ describe("ensure webhook operation", function test() {
       }
     });
 
-    minihull.post("http://localhost:8000/sync?ship=595103c73628d081190000f6&organization=localhost:8001&secret=123")
+    minihull.postConnector("595103c73628d081190000f6", "http://localhost:8000/sync")
       .then((res) => {})
   });
 
