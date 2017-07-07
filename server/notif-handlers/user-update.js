5e7c4759a9b00fe501802077e92f4e806d176b54
mkawalek <michalkawalekdevkit@icloud.com>
2017-07-07 09:22:28 +0200

    ship keyword replaced from logs with connector keyword

--- server/notif-handlers/user-update.js
+++ server/notif-handlers/user-update.js
@@ -5,7 +5,7 @@ export default function userUpdate(ctx, messages) {
   const { syncAgent } = ctx.service;
   const { logger } = ctx.client;
   if (!syncAgent.isConfigured()) {
-    logger.error("connector.configuration.error", { errors: "ship is not configured" });
+    logger.error("connector.configuration.error", { errors: "connector is not configured" });
     return Promise.resolve();
   }
   logger.debug("MESSAGES", messages.length);
