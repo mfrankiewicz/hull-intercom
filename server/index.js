077dd8b0729c977f7492b6378c0071f0fc0ad020
Sven Maschek <sven.maschek@outlook.de>
2017-08-22 23:07:47 -0400

    Kue watchStuckJobs on node process init

--- server/index.js
+++ server/index.js
@@ -35,6 +35,11 @@ const queue = new Queue("kue", {
   prefix: KUE_PREFIX,
   redis: REDIS_URL
 });
+try {
+  queue.adapter.queue.watchStuckJobs();
+} catch (e) {
+  console.error(e);
+}
 
 const connector = new Hull.Connector({ queue, cache, hostSecret: SECRET, port: PORT });
 const app = express();
