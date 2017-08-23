ff0d48d420e9925970b8b9ba489c9b43245d25d6
Michal Raczka <michal@raczka.me>
2017-08-23 12:37:01 +0200

    Add user context

--- server/notif-handlers/user-update.js
+++ server/notif-handlers/user-update.js
@@ -8,14 +8,13 @@ export default function userUpdate(ctx, messages) {
     logger.error("connector.configuration.error", { errors: "connector is not configured" });
     return Promise.resolve();
   }
-  logger.debug("MESSAGES", messages.length);
   const leads = [];
   const leadsToConvert = [];
   const users = messages.reduce((accumulator, message) => {
     const { user, changes = {}, segments = [], events = [] } = message;
     const { left = [], entered = [] } = _.get(changes, "segments", {});
 
-    logger.debug("outgoing.user.start", { changes, events: _.map(events, e => e.event) });
+    ctx.client.asUser(_.pick(user, ["email", "id", "external_id"])).logger.debug("outgoing.user.start", { changes, events: _.map(events, e => e.event) });
 
     if (!_.isEmpty(_.get(changes, "user['traits_intercom/id'][1]"))
       || !_.isEmpty(_.get(changes, "user['traits_intercom/tags'][1]"))) {
