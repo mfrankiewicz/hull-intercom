2558d4b72dce13e5aff1f4c9a5a7bdd5fd546730
Michal Raczka <michal@raczka.me>
2017-11-23 10:44:28 +0100

    Log actual traits after success in posting to firehose

--- server/jobs/save-users.js
+++ server/jobs/save-users.js
@@ -20,8 +20,9 @@ export default function saveUsers(ctx, payload) {
     const traits = syncAgent.userMapping.getHullTraits(intercomUser);
     if (ident.email) {
       const asUser = ctx.client.asUser(ident);
-      asUser.logger.info("incoming.user.success");
-      return asUser.traits(traits);
+      return asUser.traits(traits)
+        .then(() => asUser.logger.info("incoming.user.success", { traits }))
+        .catch(error => asUser.logger.error("incoming.user.error", { error }));
     }
     return ctx.client.asUser(ident).logger.info("incoming.user.skip", { reason: "missing email in ident" });
   }).then(() => {
