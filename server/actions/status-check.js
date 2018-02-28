fea2e869ba408630f5908fcdd2b31a99cf203cee
Michal Raczka <michal@raczka.me>
2018-02-28 10:25:28 +0100

    Upgrade status check

--- server/actions/status-check.js
+++ server/actions/status-check.js
@@ -1,8 +1,10 @@
 /* @flow */
 import type { $Response } from "express";
-import _ from "lodash";
 
-export default function (req: Object, res: $Response) {
+const Promise = require("bluebird");
+const _ = require("lodash");
+
+function statusCheck(req: Object, res: $Response) {
   const {
     segments = [],
     ship = {},
@@ -12,6 +14,7 @@ export default function (req: Object, res: $Response) {
   const messages = [];
   let status = "ok";
   const promises = [];
+  const audit = [];
 
   if (!_.get(ship, "private_settings.access_token")) {
     messages.push("Missing access token");
@@ -43,10 +46,10 @@ export default function (req: Object, res: $Response) {
         }
         status = "error";
       }));
-
     promises.push(service.intercomAgent.intercomClient.get("/tags")
       .then(({ body }) => {
         const mapping = _.get(ship, "private_settings.tag_mapping");
+        const promises2 = [];
         _.forEach(mapping, (tagId, segmentId) => {
           const segment = _.find(segments, { id: segmentId });
           const tag = _.find(body.tags, { id: tagId });
@@ -54,12 +57,31 @@ export default function (req: Object, res: $Response) {
             messages.push(`Not found tag: ${tagId} mapped to segment: ${segmentId} (${segment.name})`);
             status = "error";
           }
+          if (segment !== undefined && _.includes(_.get(ship, "private_settings.synchronized_segments", []), segmentId)) {
+            promises2.push(service.intercomAgent
+              .intercomClient.get("/users").query({ tag_id: tagId, per_page: 1 })
+              .then((res) => {
+                return audit.push({
+                  segmentId,
+                  tagId,
+                  name: segment.name,
+                  hullCount: segment.stats.users,
+                  intercomCount: res.body.pages.total_pages,
+                  percentage: segment.stats.users === 0
+                    ? 0
+                    : res.body.pages.total_pages / segment.stats.users * 100
+                });
+              }));
+          }
         });
+        return Promise.all(promises2);
       }));
   }
 
   Promise.all(promises).then(() => {
-    res.json({ status, messages });
+    res.json({ status, messages, audit: _.sortBy(audit, ["name"]) });
     return client.put(`${ship.id}/status`, { status, messages });
   });
 }
+
+module.exports = statusCheck;
